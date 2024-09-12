1f9f27c8d5ee79458400d467e99d5a7cfb215797
Gendozzzz <sunflowermaina@gmail.com>
2024-09-12 21:20:05 +0300

    Window and renderer

--- inc/maze.h
+++ inc/maze.h
@@ -1,2 +1,16 @@
 #ifndef MAZE_H
 #define MAZE_H
+
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+#include <SDL2/SDL_image.h>
+
+/*Global Variables*/
+extern SDL_Window *window;
+extern SDL_Renderer *renderer;
+
+bool SDL(SDL_Window **window, SDL_Renderer **renderer);
+void closeSDL(SDL_Window *window, SDL_Renderer *renderer);
--- src/window.c
+++ src/window.c
@@ -1,1 +1,85 @@
-#include "../
+#include "../inc/maze.h"
+/**
+ * SDL - initializes SDL and creates a window and renderer
+ * @window: pointer to window
+ * @renderer: pointer to renderer
+ * Return: true in successfull otherwise false
+*/
+
+bool SDL(SDL_Window **window, SDL_Renderer **renderer)
+{
+	/*Initialize SDL*/
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize SDL_Error: %s\n" SDL_GetError());
+		return (false);
+	}
+	/*Create window*/
+	*window = SDL_CreateWindow("The Maze", SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (!*window)
+	{
+		printf("Could not create window SDL_Error: %s\n" SDL_GetError());
+		return (false);
+	}
+	else
+		*renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+
+	if (!*renderer)
+	{
+		printf("Could not create renderer SDL Error: %s\n", SDL_GetError());
+		return (false);
+	}
+	return (true);
+}
+
+/**
+ * main - The maze entry point
+ * Return: 0 or 1
+ */
+
+int main(void)
+{
+	SDL_Window *window = NULL;
+	SDL_Renderer *renderer = NULL;
+
+	if (!SDL(&window, &renderer))
+	{
+		printf("Initialisation failed\n");
+		return (1);
+	}
+	bool quit = false;
+	SDL_Event e;
+
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e) != 0)
+
+		{
+			if (e.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+
+		}
+
+		/*Add functions*/
+		SDL_Delay(16);
+		closeSDL();
+	}
+
+	closeSDL(window, renderer);
+	return (0);
+
+/**
+ * closeSDL - clearup memory
+ * @window: pointer to window
+ * @renderer: pointer to renderer
+*/
+
+void closeSDL(SDL_Window *window, SDL_Renderer *renderer)
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+}
